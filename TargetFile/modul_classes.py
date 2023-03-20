124ab62ccb047c7a0de0c5c7453aa179481ff784
SERHII MELNYK <melser68@i.ua>
2023-03-20 16:16:16 +0100

    Update modul_classes.py

--- TargetFile/modul_classes.py
+++ TargetFile/modul_classes.py
@@ -5,18 +5,18 @@ class Name:
     __surname = ''
 
     @property
-    @classmethod
     def contact_name(cls):
         return cls.__name, ' ', cls.__surname
 
-    @contact_name.__setattr__
+    @contact_name.setter
     def contact_name(cls, name, surname):
         cls.__name = name
         cls.__surname = surname
 
 class Fields(Name):
-    name = Name.name
+    name = 
     surname = Name.surname
     phone = ''
     email = ''
 
+name = Name()

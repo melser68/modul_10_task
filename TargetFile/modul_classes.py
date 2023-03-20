c523fda160102406493af76c14459a8af7cabf3f
SERHII MELNYK <melser68@i.ua>
2023-03-20 23:37:35 +0100

    beta resultat

--- TargetFile/modul_classes.py
+++ TargetFile/modul_classes.py
@@ -1,22 +1,126 @@
+from string import ascii_letters
+from collections import UserDict
+
+UA_LETTERS = "йцукенгшщзхїфівапролджєячсмитьбю'"
+UA_LETTERS_UP = UA_LETTERS.upper()
+LETTERS = ascii_letters + UA_LETTERS + UA_LETTERS_UP + ' '
 
 
-class Name:
-    __name = ''
-    __surname = ''
+class Fields:
+    """Можна буде взяти статистику по номерам телефонів"""
+    f_name = []
+    f_phone = []
+    f_email = []
+
+
+class Name(Fields):    
 
     @property
-    def contact_name(cls):
-        return cls.__name, ' ', cls.__surname
+    def contact_name(self):
+        return self.name
 
     @contact_name.setter
-    def contact_name(cls, name, surname):
-        cls.__name = name
-        cls.__surname = surname
+    def contact_name(self, name):
+        Record.fill_phonebook()        
 
-class Fields(Name):
-    name = 
-    surname = Name.surname
-    phone = ''
-    email = ''
+        if len(name.strip(LETTERS)) == 0:
+            if len(name) > 0:
+                self.name = name
+                self.f_name.append(name)
+                return True
+                
+            else:
+                print('name must contain at least one character')
+                return False
+        else:
+            print('Name can only contain english or ukrainian letters and spaces')
+            return False
+    
+    @contact_name.deleter
+    def contact_name(self):
+        self.f_name.remove(self.name)
+        del self.name        
+        
 
-name = Name()
+
+class Phone(Fields):  
+    
+
+    @property
+    def number(self):
+        return (', ').join(self.phone)
+
+    @number.setter
+    def number(self, number):
+
+        if len(number) == 13 and number[0] == '+':
+            try:
+                self.phone.append(number)                
+            except AttributeError:
+                self.phone = [number]
+            self.f_phone.append(number)                
+            if self.data.get(self.name) == None:
+                self.data.update({self.name: {'phone':self.phone}})
+            Record.add_record(self)
+            
+            return True               
+        else:
+            print('The entered number does not match the pattern')
+            return False
+    
+    
+    def del_number(self, number):
+        self.f_phone.remove(number)
+        self.phone.remove(number)
+        if len(self.phone)  == 0:
+            self.phonebook.update({self.name: {'phone':'no number phone'}})
+        Record.add_record(self)
+    
+
+
+
+
+class PhoneBook(UserDict, Phone, Name):
+    phonebook = UserDict()    
+
+
+class Record(PhoneBook):
+
+    def change_name(self, name_contact):
+        res_search = {}
+        for kontakt, list_num in self.phonebook.items():
+            if name_contact.lower() in kontakt.lower():
+                res_search.update({kontakt: list_num})
+
+        return res_search
+
+    def add_record(self):    
+        with open('save_book.msf', 'w') as fh:
+            for i, y in self.data.items():
+                for a, b in y.items():
+                    fh.write(i + ' : ' + (',').join(b) + '\n')
+    @classmethod
+    def fill_phonebook(cls):
+        if len(cls.phonebook) == 0:
+            with open('save_book.msf') as fh:
+                while True:
+                    a = fh.readline()
+                    if not a:
+                        break
+                    else:
+                        cls.phonebook.update(
+                        {a[:-1].split(' : ')[0]: {'phone': a[:-1].split(' : ')[1].split(',')}})
+    @classmethod
+    def print_all (cls):
+        cls.fill_phonebook() 
+        for i, y in PhoneBook.phonebook.data.items():
+            for a, b in y.items():
+                print(i + ' : ' + (',').join(b))
+
+
+
+
+
+
+
+Record.print_all()
